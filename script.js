8dc9de11d261ac5eeb270e99520665ff0280b1f7
Harshada Khandu Abhale <hkabhale99@gmail.com>
2021-09-18 11:03:58 +0530

    bug fixed

--- script.js
+++ script.js
@@ -3,94 +3,69 @@ const form = document.getElementById("form");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const mobileno = document.getElementById("mobileno");
-const password = document.getElementById("password");
+const password = document.querySelector("#password");
 const togglePassword = document.querySelector("#togglePassword");
 const cfmpassword = document.getElementById("cfmpassword");
 const gender = document.getElementById("gender");
 
-//add event
 
+//add event
 form.addEventListener("submit",(event) =>{
     event.preventDefault();
-    validate();
-})
-
-const sendData = (usernameVal, sRate, count) => {
-    if(sRate === count){
+    if(usernameValid() == true && emailValid() == true && mblValid() == true && passValid() == true && cfmpassValid() == true)
+    {
         alert("Registration Successful");
-        swal("Welcome! "+usernameVal, "Regsitration Successful" , "success");
+        swal("Welcome! ", "Regsitration Successful" , "success");
     }
-}
+});
 
-//for final data validation
-const successMsg = (usernameVal) => {
-    let formCont = document.getElementsByClassName("form-control");
-    var count = formCont.length - 1;
-    for(var i=0; i<formCont.length; i++) {
-        if(formCont[i].className === "form-control success"){
-            var sRate = 0 + i;
-            sendData(usernameVal, sRate, count);
-        }else{
-            return false;
-        }
-    }
-}
-
-
-//more email validation
-const isEmail = (emailVal) => {
-    var atSymbol = emailVal.indexOf("@");
-    if(atSymbol < 1) return false;
-    var dot = emailVal.lastIndexOf(".");
-    if(dot <= atSymbol + 3) return false;
-    if(dot === emailVal.length - 1) return false;
-    return true;
-}
-
-// Define the validate function
-
-
-    const usernameVal = username.value.trim();
-    const emailVal = email.value.trim();
-    const mobilenoVal = mobileno.value.trim();
-    const passwordVal = password.value.trim();
-    const cfmpasswordVal = cfmpassword.value.trim(); 
-    const genderVal = gender.value.trim();
-    
-    successMsg(usernameVal);
-
-
-    function usernameValid(event){
-        let usernameVal = event.target.value;
+    function usernameValid(){
+        var usernameVal = document.getElementById("username").value;
     
         //validate username 
         if(usernameVal === ""){
             setErrorMsg(username, "username cannot be blank.");
+            return false;
         }
         else if(usernameVal.length <= 2){
             setErrorMsg(username, "minimum 3 characters");
+            return false;
         }else{
             setSuccessMsg(username);
+            return true;
         }
         
     }
     
-    function emailValid(event){
-        let emailVal = event.target.value;
+    function emailValid(){
+        let emailVal = document.getElementById("email").value;
+        //more email validation
+        const isEmail = (emailVal) => {
+            var atSymbol = emailVal.indexOf("@");
+            if(atSymbol < 1) return false;
+            var dot = emailVal.lastIndexOf(".");
+            if(dot <= atSymbol + 3) return false;
+            if(dot === emailVal.length - 1) return false;
+            return true;
+        }
     
         //validate email
         if(emailVal === ""){
             setErrorMsg(email, "email cannot be blank.");
+            return false;
         }
         else if(!isEmail(emailVal)){
             setErrorMsg(email, "Not a valid Email.");
+            return false;
         }else{
             setSuccessMsg(email);
-        }
+            return true;
+        }        
+
     }
     
-    function mblValid(event){
-        let mobilenoVal = event.target.value;
+    function mblValid(){
+        let mobilenoVal = document.getElementById("mobileno").value;
     
         // validate Mobile number
         if(mobilenoVal === ""){
@@ -101,50 +76,51 @@ const isEmail = (emailVal) => {
         }else{
             setSuccessMsg(mobileno);
         }
+        return true;
     }
     
-    function passValid(event){ 
-        let passwordVal= event.target.value;
+    function passValid(){ 
+        let passwordVal= document.getElementById("password").value;
 
         // validate password
         if(passwordVal === ""){
             setErrorMsg(password, "Password cannot be blank.");
+            return false;
         }
         else if(passwordVal.length <= 5){
             setErrorMsg(password, "Minimum 6 characters.");
+            return false;
         }else if(passwordVal.length >= 15){
             setErrorMsg(password, "Less than 15 characters.");
+            return false;
         }else{
             setSuccessMsg(password);
+            return true;
         }
+        
     }
     
-    function cfmpassValid(event){
-        let cfmpasswordVal = event.target.value;
-    
+    function cfmpassValid(){
+        let cfmpasswordVal = document.getElementById("cfmpassword").value;
+        let password1 = document.getElementById("password").value;
         //validate confirm password
         if(cfmpasswordVal === ""){
             setErrorMsg(cfmpassword, "Confirm Password cannot be blank.");
+            return false;
         }
-        else if(passwordVal != cfmpasswordVal){
+        else if(password1 != cfmpasswordVal){
             setErrorMsg(cfmpassword, "Password must be same as above.");
+            return false;
         }else{
             setSuccessMsg(cfmpassword);
+            return true;
         }
+        
     }
-
-
-function genderValid(event){
-    if(genderVal ===""){
-        setErrorMsg(gender, "Gender cannot be blank.");
-    }else{
-        setSuccessMsg(gender);
-    }
-}
-
+    
 togglePassword.addEventListener("click", function(e){
     const type = password.getAttribute("type") === "password" ? "text" : "password";
-    password.setAttribute("type", type);
+    password.setAttribute("type", type);   
     this.classList.toggle("fa-eye");
 });
 
